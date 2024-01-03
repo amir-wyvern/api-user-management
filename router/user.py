9d1fa670116a037f2830fc315f041fe214007a9f
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-03 13:26:03 -0500

    feat: add grpc session in Depend routes

--- router/user.py
+++ router/user.py
@@ -1,5 +1,6 @@
 from fastapi import (
-    APIRouter
+    APIRouter,
+    Depends
 )
 from schemas import (
     BaseResponse,
@@ -8,20 +9,14 @@ from schemas import (
     UserUpdateInfo,
     UserInfoResponse
 )
+from database_service.session import get_grpc
+from grpc_utils.database_pb2_grpc import DataBaseStub
 import grpc_utils.database_pb2 as pb2
-import grpc
-import grpc_utils.database_pb2_grpc as pb2_grpc
-import os 
 
 router = APIRouter(prefix='/user', tags=['User'])
 
-PORT = os.getenv("GRPC_PORT")
-HOST = os.getenv("GRPC_HOST")
-
-chanel = grpc.insecure_channel(f'{HOST}:{PORT}')
-stub = pb2_grpc.DataBaseStub(chanel)
 @router.get('/info', response_model= UserInfoResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def create_new_user(username: str):
+def create_new_user(username: str, stub: DataBaseStub = Depends(get_grpc)):
 
     data = {
             'name': 'test',
@@ -33,7 +28,7 @@ def create_new_user(username: str):
     return UserInfoResponse(**data)
 
 @router.post('/new', response_model= BaseResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def create_new_user(request: UserRegister):
+def create_new_user(request: UserRegister, stub: DataBaseStub = Depends(get_grpc)):
 
     data = {
         'name': request.name,
@@ -44,26 +39,27 @@ def create_new_user(request: UserRegister):
         'role': request.role
     }
     
+    stub.NewUser(pb2.RequestNewUser(**data))
     
     return BaseResponse(message= 'message test', code= 1200)
 
 @router.put('/info/edit', response_model= BaseResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def edit_user_information(request: UserUpdateInfo):
+def edit_user_information(request: UserUpdateInfo, stub: DataBaseStub = Depends(get_grpc)):
 
     return BaseResponse(message= 'message test', code= 1200)
 
 @router.put('/pass/edit', response_model= BaseResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def change_user_password(request: UserUpdateInfo):
+def change_user_password(request: UserUpdateInfo, stub: DataBaseStub = Depends(get_grpc)):
 
     return BaseResponse(message= 'message test', code= 1200)
 
 @router.put('/role/edit', response_model= BaseResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def change_user_role(request: UserUpdateInfo):
+def change_user_role(request: UserUpdateInfo, stub: DataBaseStub = Depends(get_grpc)):
 
     return BaseResponse(message= 'message test', code= 1200)
 
 @router.delete('/delete', response_model= BaseResponse, responses= {404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def delete_user(request: UserUpdateInfo):
+def delete_user(request: UserUpdateInfo, stub: DataBaseStub = Depends(get_grpc)):
 
     return BaseResponse(message= 'message test', code= 1200)
 
