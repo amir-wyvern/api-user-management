b26ce631ece95a432a9f1db756a3dc565cb48344
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-03 20:24:45 -0500

    fix: use raise instead of return in HttpExceptions

--- router/auth.py
+++ router/auth.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
-from schemas import Token
+from schemas import Token, HTTPError
 from auth.auth import verify_password, create_access_token
 from database_service.session import get_grpc
 from grpc_utils.database_pb2_grpc import DataBaseStub
@@ -26,41 +26,38 @@ logger.addHandler(console_handler)
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
-@router.post("/login", response_model=Token)
+@router.post("/login", response_model=Token, responses= {401:{'model':HTTPError}, 500:{'model':HTTPError}})
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], stub: DataBaseStub= Depends(get_grpc)):
     
     if form_data.scopes and 'admin' in form_data.scopes :
-        
         scope = 'admin'
 
     elif form_data.scopes and 'user' in form_data.scopes:
-        
         scope = 'user'
 
     else : 
-        return HTTPException(
+        raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Unkown Scopes",
         )
     try:
-
-        resp_user = stub.GetUser(pb2.RequestGetUser(**{'username': form_data.username}))
+        
+        resp_user = stub.GetUser(pb2.RequestUserInfo(**{'username': form_data.username}))
 
         if resp_user.code != 1200 :
-            return HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password')
-
+            raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password')
 
     except Exception as e:
         logger.error(f'[login] error in grpc connection (user:{form_data.username} -error: {e})')
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='check the logs for more info')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='check the logs for more info')
     
     if resp_user.data.role.lower() == scope:
-        return HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password')
+        raise HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail='Incorrect username or password')
     
     check_password = verify_password(resp_user.data.password, form_data.password )
 
     if not check_password:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
     
     access_token = create_access_token(
         data={"user": resp_user.data.user_id, 'role': resp_user.data.role, "scopes": scope},
